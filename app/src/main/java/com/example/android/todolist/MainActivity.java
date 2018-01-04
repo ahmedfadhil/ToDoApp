bb3ce9707d2f628aa099e4a2eef36002db8a099f
ahmed fadhil <fadhil@fbk.eu>
2018-01-04 15:52:57 +0100

    bugs fixed

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.android.todolist;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,82 +18,98 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
 
-    static Set<String> set;
-    ListView listView;
-    static ArrayList<String> notes = new ArrayList<String>();
+public class MainActivity extends Activity {
+
+    static ArrayList<String> notes = new ArrayList<>();
     static ArrayAdapter arrayAdapter;
+    static Set<String> set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView) findViewById(R.id.listView);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setTitle("Are you sure?")
-                        .setMessage("Are you sure dude, this is serious")
-                        .setPositiveButton("Yup", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int position) {
-                                notes.remove(position);
-                                SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences("com.example.android.todolist", Context.MODE_PRIVATE);
-                                if (set == null) {
-                                    set = new HashSet<String>();
-                                } else {
-                                    set.clear();
-                                }
-                                set.addAll(notes);
-                                sharedPreferences.edit().remove("notes").apply();
-                                sharedPreferences.edit().putStringSet("notes", set).apply();
-                                    arrayAdapter.notifyDataSetChanged();
-                            }
-                        })
-                        .setNegativeButton("Nah", null)
-                        .show();
+        ListView listView = (ListView) findViewById(R.id.listView);
 
-
-            }
-        });
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.android.todolist", Context.MODE_PRIVATE);
+
         set = sharedPreferences.getStringSet("notes", null);
 
         notes.clear();
+
         if (set != null) {
+
             notes.addAll(set);
+
         } else {
+
             notes.add("Example note");
             set = new HashSet<String>();
             set.addAll(notes);
             sharedPreferences.edit().putStringSet("notes", set).apply();
+
         }
 
+
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
         listView.setAdapter(arrayAdapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 Intent i = new Intent(getApplicationContext(), EditYourNotes.class);
                 i.putExtra("noteId", position);
                 startActivity(i);
+
+
+            }
+
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                notes.remove(position);
+
+                                SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences("com.example.android.todolist", Context.MODE_PRIVATE);
+
+                                if (set == null) {
+
+                                    set = new HashSet<String>();
+
+                                } else {
+
+                                    set.clear();
+
+                                }
+
+                                set.addAll(notes);
+                                sharedPreferences.edit().remove("notes").apply();
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                                arrayAdapter.notifyDataSetChanged();
+
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true;
             }
         });
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
@@ -113,20 +128,32 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.add) {
+
             notes.add("");
+
             SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.android.todolist", Context.MODE_PRIVATE);
+
             if (set == null) {
+
                 set = new HashSet<String>();
+
             } else {
+
                 set.clear();
+
             }
+
             set.addAll(notes);
-            sharedPreferences.edit().putStringSet("notes", set).apply();
             arrayAdapter.notifyDataSetChanged();
+
             sharedPreferences.edit().remove("notes").apply();
+            sharedPreferences.edit().putStringSet("notes", set).apply();
+
             Intent i = new Intent(getApplicationContext(), EditYourNotes.class);
             i.putExtra("noteId", notes.size() - 1);
             startActivity(i);
+
+
             return true;
         }
 
